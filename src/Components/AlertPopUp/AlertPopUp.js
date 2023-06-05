5d391eac8609b16c28727c7b21eeca4bfef0d36b
aeloh1m <melowgz@gmail.com>
2023-06-05 14:02:04 -0300

    Adding universal Alert pop up

---
+++ src/Components/AlertPopUp/AlertPopUp.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+
+/*
+    Para utilizarlo simplemente añadir el AlertPopUp 
+    pasando por props título y mensaje, un useState:
+    const [openPopUp, setOpenPopUp] = useState(false);
+    dentro del componente que se desee agregar un alert
+    y luego en el condicional:
+            setTimeout(() => {
+          setOpenPopUp(true)
+        }, 0);
+    
+*/
+
+const AlertPopUp = ({ open, onClose, mensaje, titulo }) => {
+
+    if (!open) return null;
+
+    return (
+        <>
+            <Modal show={true}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{titulo}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{mensaje}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={onClose}>
+                        Cerrar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+
+    );
+};
+
+export default AlertPopUp
+
+

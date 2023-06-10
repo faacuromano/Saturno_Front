99f89f249248663633e120c608e5c0ffda0510b2
aeloh1m <melowgz@gmail.com>
2023-06-10 14:57:08 -0300

    WIP: Send email with code

--- src/Components/ValidatePayment/ValidatePayment.js
+++ src/Components/ValidatePayment/ValidatePayment.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
+import React, { useState, useRef } from 'react'
+import emailjs from '@emailjs/browser';
+import { Button, Form, Modal } from 'react-bootstrap'
 import { GetByProfUsername } from '../../functions/professionalMethods'
 
 const ValidatePayment = () => {
 
+  const form = useRef();
   const [loggedUser1, setLoggedUser1] = useState();
 
-  
-  
   const returnInfo = () => {
-    
+
     const loggedUser = localStorage.getItem("user");
 
     GetByProfUsername(loggedUser).then((response) => {
       const mail = response.mail
       console.log(mail)
-    } );
+    });
 
   }
 
+  const sendEmailValidation = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_bi7vwtq', 'template_xwpoj3g', form.current, 'RERaAk2SC1-ojAP5A')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    {
+    }
+
+  };
+  console.log(form.current)
   // authClient(userName, password).then(function (response) {
   //   if (response) {
   //     localStorage.setItem("user", response.data.user.username);
@@ -31,8 +45,19 @@ const ValidatePayment = () => {
 
   return (
     <div>
-        <Button className='button' show={true} variant="primary" onClick={returnInfo} >Validar pago</Button>
-        
+      <Button className='button' show={true} variant="primary" onClick={sendEmailValidation} >Validar pago</Button>
+      <Modal show={true}>
+        <form ref={form} onSubmit={sendEmailValidation}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="saymynameisblack@gmail.com" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </Modal>
+
     </div>
 
   )
